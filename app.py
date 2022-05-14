6ecf7fd8ffd7ceacf25b56672d25b0b5584e8270
Angelopezmacc <angel@FLE-Windows>
2022-05-14 07:59:30 -0500

    Avance signup y login

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import request
+
 import os
 
 dbdir = "sqlite:///" + os.path.abspath(os.getcwd()) + "/database.db"
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = dbdir
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-# class Posts(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(50))
-class Users(db.Model)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True , nullable=False)
+    password = db.Column(db.String(80), nullable = False)
+
+@app.route("/signup", methods = ["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        hashed_pw = generate_password_hash(request.form["password"], method="sha256")
+        new_user = Users(username=request.form["username"], password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return "Usted se ha registrado correctamente"
+
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
